5646c8cec6f8c56b4af837c7f12ba71c86ca67e4
MrQuba <mrquba4@gmail.com>
2025-04-30 08:30:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple bullet hell game
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+  auto window =
+      sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Bullet Hell Game");
+  window.setFramerateLimit(60);
+
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
+    }
+
+    window.clear();
+    window.display();
+  }
+}
